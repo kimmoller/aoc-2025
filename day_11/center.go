b4b5362df46abc63e591b0d18ddd69c35d6a945c
Kim Möller <kim.moller2@protonmail.con>
2026-01-10 20:59:58 +0100

    Revert "Refactor lists to strings to optimize part 2 verification"
    This reverts commit e2d785da006d4d88f205cf3df04862bb1c92b341.

--- day_11/center.go
+++ day_11/center.go
@@ -32,15 +32,23 @@ func (c *Center) Server(name string) (*Server, error) {
 	return nil, fmt.Errorf("server %s not found", name)
 }
 
-func (c *Center) FindAllPaths(start, end string, strict bool) ([]string, error) {
-	allPaths := []string{}
+func (c *Center) FindAllPaths(start, end string, strict bool) ([][]string, error) {
+	allPaths := [][]string{}
 	startingServer, err := c.Server(start)
 	if err != nil {
 		return nil, err
 	}
-	err = c.FindPath(&allPaths, "", startingServer.name, end)
+	for _, link := range startingServer.links {
+		path := []string{startingServer.name}
+		output := link.output
+		err := c.FindPath(&allPaths, path, output, end)
+		if err != nil {
+			return nil, err
+		}
+	}
 
-	finalPaths := []string{}
+	spew.Dump(fmt.Sprintf("Number of paths before check %d", len(allPaths)))
+	finalPaths := [][]string{}
 	if strict {
 		for _, path := range allPaths {
 			if meetsRequirements(path) {
@@ -54,18 +62,21 @@ func (c *Center) FindAllPaths(start, end string, strict bool) ([]string, error) 
 	return finalPaths, nil
 }
 
-func (c *Center) FindPath(finalPaths *[]string, currentPath string, currentServerName string, finalServerName string) error {
-	path := appendToPath(currentPath, currentServerName)
+func (c *Center) FindPath(finalPaths *[][]string, currentPath []string, nextServerName string, finalServerName string) error {
+	path := currentPath
+	path = append(path, nextServerName)
 
-	server, err := c.Server(currentServerName)
+	server, err := c.Server(nextServerName)
 	if err != nil {
 		return err
 	}
 
+	// This needs to somehow handle the branching paths
+	// We cannot just return here. Instead it has to create a new path per new link and somehow return all paths once they are done
 	for _, link := range server.links {
 		output := link.output
 		if output == finalServerName {
-			path = appendToPath(path, output)
+			path = append(path, output)
 			*finalPaths = append(*finalPaths, path)
 		} else {
 			err := c.FindPath(finalPaths, path, output, finalServerName)
@@ -78,14 +89,17 @@ func (c *Center) FindPath(finalPaths *[]string, currentPath string, currentServe
 	return nil
 }
 
-func appendToPath(path string, serverName string) string {
-	newPath := path + "," + serverName
-	return newPath
-}
-
-func meetsRequirements(path string) bool {
-	hasDac := strings.Contains(path, "dac")
-	hasFft := strings.Contains(path, "fft")
+func meetsRequirements(path []string) bool {
+	hasDac := false
+	hasFft := false
+	for _, server := range path {
+		if server == "dac" {
+			hasDac = true
+		}
+		if server == "fft" {
+			hasFft = true
+		}
+	}
 
 	return hasDac && hasFft
 }
